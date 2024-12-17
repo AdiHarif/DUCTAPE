b27d1f0bc4d589a6ccb3d8954c011d8697a605c1
Adi Harif <adi.harif@campus.technion.ac.il>
2024-12-17 06:50:51 +0200

    Updates sudoku benchmarks to include ductape complient test

--- tests/sudoku_benchmarks.test.ts
+++ tests/sudoku_benchmarks.test.ts
@@ -17,8 +17,8 @@ afterAll(() => {
     const outpath = "sudoku_benchmarks.csv";
     fs.writeFileSync(outpath, "File,Time(s)\n");
     for (const file in results) {
-        const time = results[file];
-        fs.appendFileSync(outpath, `${file},${time}\n`);
+        const [tsTime, nativeTime] = results[file];
+        fs.appendFileSync(outpath, `${file},${tsTime},${nativeTime}\n`);
     }
 });
 
@@ -34,7 +34,7 @@ function testCppFile(cppFile: string) {
     expect(output).toEqual("");
 
     const cppFileBase = cppFile.split('/').pop()!;
-    results[cppFileBase] = time;
+    results[cppFileBase] = ['-', time];
 }
 
 function testTsFile(tsFile: string) {
@@ -50,7 +50,30 @@ function testTsFile(tsFile: string) {
     expect(output).toEqual("");
 
     const tsFileBase = tsFile.split('/').pop()!;
-    results[tsFileBase] = time;
+    results[tsFileBase] = [time, '-'];
+}
+
+function testDuctapeFile(ductapeFile: string) {
+    const outDir = 'out';
+    execSync(`tsc --outDir ${outDir} --skipLibCheck ${ductapeFile} --target ESNext`);
+    const tsStartTime = process.hrtime();
+    const expectedOutputBuffer = execSync(`node ${outDir}/* ${inputFile} ${iterations}`);
+    const endTime = process.hrtime(tsStartTime);
+    const tsTime = endTime[0] + endTime[1] / 1e9;
+    const output = expectedOutputBuffer.toString();
+    expect(output).toEqual("");
+
+    const execFile = 'out/a.out';
+    execSync(`npm start -- -i ${ductapeFile} -o ${execFile}`);
+    const nativeStartTime = process.hrtime();
+    const nativeOutputBuffer = execSync(`${execFile} ${inputFile} ${iterations}`);
+    const nativeEndTime = process.hrtime(nativeStartTime);
+    const nativeTime = nativeEndTime[0] + nativeEndTime[1] / 1e9;
+    const nativeOutput = nativeOutputBuffer.toString();
+    expect(nativeOutput).toEqual("");
+
+    const ductapeFileBase = ductapeFile.split('/').pop()!;
+    results[ductapeFileBase] = [tsTime, nativeTime];
 }
 
 
@@ -58,7 +81,8 @@ describe("Sudoku Benchmarks (based on LangBench", () => {
     const dir = "tests/sudoku_benchmarks";
     const files = fs.readdirSync(dir);
     const cppFiles = files.filter(f => f.endsWith('.cpp'));
-    const tsFiles = files.filter(f => f.endsWith('.ts'));
+    const tsFiles = files.filter(f => f.endsWith('.ts')).filter(f => !f.endsWith('.ductape.ts'));
+    const ductapeFiles = files.filter(f => f.endsWith('.ductape.ts'));
 
     for (const file of cppFiles) {
         test(file, () => {
@@ -69,6 +93,12 @@ describe("Sudoku Benchmarks (based on LangBench", () => {
     for (const file of tsFiles) {
         test(file, () => {
             testTsFile(`${dir}/${file}`);
+        });
+    }
+
+    for (const file of ductapeFiles) {
+        test(file, () => {
+            testDuctapeFile(`${dir}/${file}`);
         });
     }
 });
